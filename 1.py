f66185bd4e065a562ef03bd62d85472cbc3cdfda
BhargaviSharma3074 <118887063+BhargaviSharma3074@users.noreply.github.com>
2022-12-09 15:01:12 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,20 @@
+#use of arithmatic operators
+a = int(input("Enter the value in a: "))
+b = int(input("Enter the value in b: "))
+
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a//b
+h = a%b
+i = a**3
+
+print("a+b = ",c)
+print()#empty print function
+print("a-b = ",d)
+print("a*b = ",e)
+print("a/b = ",f)
+print("a//b = ",g)
+print("a%b = ",h)
+print("a**3 = ",i)
